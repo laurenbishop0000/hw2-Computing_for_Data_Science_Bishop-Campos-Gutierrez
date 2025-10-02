4f161b801411240594bb9eabe9e100b46ed61bc9
“laurenbishop0000” <laurenbishop0000@gmail.com>
2025-10-02 14:30:50 +0200

    Commiting first set of hw2_first_questions

---
+++ hw2_first_questions.py
@@ -0,0 +1,33 @@
+# 1)
+# Create a function named
+# "triple" that takes one
+# parameter, x, and returns
+# the value of x multiplied
+# by three.
+def triple(x):
+    return x * 3
+
+
+# 2)
+# Create a function named "subtract" that
+# takes two parameters and returns the result of
+# the second value subtracted from the first.
+def subtract(a, b):
+    return a - b
+
+
+# 3)
+# Create a function called "dictionary_maker"
+# that has one parameter: a list of 2-tuples.
+# It should return the same data in the form
+# of a dictionary, where the first element
+# of every tuple is the key and the second
+# element is the value.
+#
+# For example, if given: [('foo', 1), ('bar', 3)]
+# it should return {'foo': 1, 'bar': 3}
+# You should program the function and not use
+# the function "dict" directly
+def dictionary_maker(x):
+    dictionary = {x[0][0]:x[0][1], x[1][0]:x[1][1]}
+    return dictionary
