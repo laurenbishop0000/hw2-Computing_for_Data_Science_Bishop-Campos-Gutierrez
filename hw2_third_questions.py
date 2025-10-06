5751a86dfd32fe3064ba4e8aa2b9bbba13545b83
danielcampont <danielcampospont@gmail.com>
2025-10-06 14:14:56 +0200

    Daniel's commit

---
+++ hw2_third_questions.py
@@ -0,0 +1,55 @@
+# Now imagine you have a certain data structure that
+# contains information about different countries and
+# the number of people who was registered with covid
+# in a weekly basis.
+# e.g. {'Spain': [4, 8, 2, 0, 1], 'France': [2, 3, 6],
+#       'Italy': [6, 8, 1, 7]}
+# Assuming that the moment they started reporting the
+# number of registered cases is not the same (thus
+# the length of the lists can differ)
+
+
+# 7)
+# Create a function called "total_registered_cases"
+# that has 2 parameters:
+# 1) The data structure described above.
+# 2) A string with the country name.
+#
+# The function should return the total number of cases
+# registered so far in that country
+
+def total_registered_cases(data, country):
+    return sum(data[country])
+
+# 8)
+# Create a function called "total_registered_cases_per_country"
+# that has 1 parameter:
+# 1) The data structure described above.
+#
+# The function should return a dictionary with a key
+# per each country and as value the total number of cases
+# registered so far that the country had
+#
+
+def total_registered_cases_per_country(data):
+    dictionary = {}
+    for country in data:
+        dictionary[country] = total_registered_cases(data, country)
+    return dictionary
+
+# 9)
+# Create a function called "country_with_most_cases"
+# that has 1 parameter:
+# 1) The data structure described above
+#
+# The function should return the country with the
+# greatest total amount of cases
+
+def country_with_most_cases(data):
+    country_count = total_registered_cases_per_country(data)
+    most_cases = ['',0]
+    for x in country_count:
+        if(country_count[x]>most_cases[1]):
+            most_cases[1] = country_count[x]
+            most_cases[0] = x
+    return tuple(most_cases)
